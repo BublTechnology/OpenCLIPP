2ab6b75cd98d0f785760f1105fb34b88aa95be3b
Jeffrey Heifetz <jeffheifetz@gmail.com>
2015-12-01 16:24:17 -0500

    Initial work on getting the C++ Demo to work

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -3,23 +3,23 @@
 //! @date   : Jul 2013
 //!
 //! @brief  : Simple demo program for OpenCLIPP library
-//! 
+//!
 //! Copyright (C) 2013 - CRVI
 //!
 //! This file is part of OpenCLIPP.
-//! 
+//!
 //! OpenCLIPP is free software: you can redistribute it and/or modify
 //! it under the terms of the GNU Lesser General Public License version 3
 //! as published by the Free Software Foundation.
-//! 
+//!
 //! OpenCLIPP is distributed in the hope that it will be useful,
 //! but WITHOUT ANY WARRANTY; without even the implied warranty of
 //! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //! GNU Lesser General Public License for more details.
-//! 
+//!
 //! You should have received a copy of the GNU Lesser General Public License
 //! along with OpenCLIPP.  If not, see <http://www.gnu.org/licenses/>.
-//! 
+//!
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <OpenCLIPP.hpp>
@@ -68,7 +68,7 @@ int main(int /*argc*/, char ** /*argv*/)
 
    // Initialize OpenCL
    COpenCL CL;
-   CL.SetClFilesPath("D:/OpenCLIPP/cl-files/");
+   CL.SetClFilesPath("../cl-files/");
    Filters Filters(CL);
 
    // Display device name
